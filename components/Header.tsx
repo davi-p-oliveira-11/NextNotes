657c2615b4ac18ea0582c50ff2f444c91a2d6135
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-11 17:28:37 -0300

    style(header): applied tailwind classes to style header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ function Header() {
   return (
     <div className="flex items-center justify-between p-5">
       {user && (
-        <h1>
+        <h1 className="text-2xl">
           {user?.firstName}
           {`'s`} Notes
         </h1>
