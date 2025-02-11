7e21bb3ea6b31c0cf528d97742f5e19b367665a1
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-11 17:10:35 -0300

    feat(header): add auth h1 and Clerk breadcrumbs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  useUser,
+  UserButton,
+} from "@clerk/nextjs";
 
 function Header() {
-  return <div>Header</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1>
+          {user?.firstName}
+          {`'s`} Notes
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
