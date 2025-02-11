d8a068a34050deaae0a464efa4959fcfdcca77c8
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-11 16:23:08 -0300

    isntalled clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
       <h1>NextNotes</h1>
+
+      <Button>Click </Button>
     </main>
   );
 }
