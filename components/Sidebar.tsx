ee6ef79d0b415fbc14bdf0f4592f5443f836ddbb
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-12 19:34:37 -0300

    eat(ui): a dd Sheet component from shadcn/ui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,9 @@ import NewDocumentButton from "./NewDocumentButton";
 function Sidebar() {
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <NewDocumentButton />
+      <div className="hidden md:inline">
+        <NewDocumentButton />
+      </div>
     </div>
   );
 }
