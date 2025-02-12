66a0c78f520c88d4bf1e02e778ae488e0638df21
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-12 20:35:57 -0300

    added styling to te sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,51 @@
+"use client";
+import { MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 function Sidebar() {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+      {/* My Documents */}
+      {/* List ... */}
+
+      {/* Shared with Me */}
+      {/* List ...  */}
+    </>
+  );
+
   return (
     <div className="p-2 md:p-5 bg-gray-200 relative">
-      <div className="hidden md:inline">
-        <NewDocumentButton />
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:capacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
+}
+
+{
+  /* go back to 53:00 to set cloudfare if you actually want to level up your projects level */
 }
 
 export default Sidebar;
